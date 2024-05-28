c7e4ab9b4af5d97df990dab3816a1bd81bf288d4
jwolfe972 <74490269+jwolfe972@users.noreply.github.com>
2024-05-27 19:55:03 -0500

    Add files via upload

---
+++ statcast_p.py
@@ -0,0 +1,37 @@
+from pybaseball import statcast_single_game
+import pandas as pd
+from pybaseball import statcast
+from pybaseball import cache
+from datetime import datetime
+from pybaseball import playerid_reverse_lookup
+# this should get the current one inning cycles for current season
+
+
+def hit_value_column(value):
+    value_dict = {
+        'single': 1,
+        'double': 2,
+        'triple': 3,
+        'home_run': 4
+    }
+
+    return value_dict.get(value, 0)
+
+
+if __name__ == '__main__':
+    cache.enable()
+    # # get all play by play data
+    data = statcast('2024-03-28', datetime.now().strftime('%Y-%m-%d'))
+
+    play_by_play_2024 = data[
+        ['game_date', 'home_team', 'away_team', 'inning', 'inning_topbot', 'pitcher', 'events']].dropna()
+
+    play_by_play_2024['hit_value'] = play_by_play_2024['events'].apply(hit_value_column)
+
+    play_by_play_2024.to_csv(
+        '<filename.csv>',
+        index=False)
+
+    # data = playerid_reverse_lookup([641927, 607259,676775], key_type='mlbam')
+    # print(data)
+
