1467eb45fade9c6f60560d83399873acbbfeea07
jwolfe972 <74490269+jwolfe972@users.noreply.github.com>
2024-05-31 14:00:00 -0500

    Add files via upload

--- statcast_p.py
+++ statcast_p.py
@@ -21,7 +21,7 @@ def hit_value_column(value):
 if __name__ == '__main__':
     cache.enable()
     # # get all play by play data
-    data = statcast('2024-03-28', datetime.now().strftime('%Y-%m-%d'))
+    data = statcast('2024-03-20', datetime.now().strftime('%Y-%m-%d'))
 
     play_by_play_2024 = data[
         ['game_date', 'home_team', 'away_team', 'inning', 'inning_topbot', 'pitcher', 'events']].dropna()
@@ -29,9 +29,13 @@ if __name__ == '__main__':
     play_by_play_2024['hit_value'] = play_by_play_2024['events'].apply(hit_value_column)
 
     play_by_play_2024.to_csv(
-        '<filename.csv>',
+        '<enter filename>.csv',
         index=False)
 
     # data = playerid_reverse_lookup([641927, 607259,676775], key_type='mlbam')
     # print(data)
+
+    #
+    # data = playerid_reverse_lookup(['yelic001', 'bauej001', 'barnb002', 'gomec002', 'younr001', 'robif103', 'doerb101', 'gehrl101', 'robed103'], key_type='retro')
+    # print(data[['name_first', 'name_last']])
 
